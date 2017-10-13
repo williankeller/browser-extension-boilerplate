d0c5f25220d6f09c44c0dbd25f655718cf8c5022
Willian Keller <will_levinski@hotmail.com>
2017-10-13 10:57:33 -0300

    Create options scripts

---
+++ src/js/options.js
@@ -0,0 +1,25 @@
+(function (chrome, window, Selector) {
+  'use strict';
+
+  /**
+   * Open options dropown list.
+   */
+  Selector.click('.open-options', null, function () {
+    Selector.toggle('.menu-options', 'show');
+  });
+
+  /**
+   * Open the settings page.
+   */
+  Selector.click('.open-settings', null, function () {
+    if (chrome.runtime.openOptionsPage) {
+      // New way to open options pages, if supported (Chrome 42+).
+      chrome.runtime.openOptionsPage();
+    }
+    else {
+      // Reasonable fallback.
+      window.open(chrome.runtime.getURL('options.html'));
+    }
+  });
+
+})(chrome, window, Selector);
